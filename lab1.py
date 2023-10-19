aa2c9070607ef0fa53d446af2a1625aa64fd6881
Madina <abdulgafarova.madina@mail.ru>
2023-10-19 19:26:24 +0600

    lab1 second commit

---
+++ lab1.py
@@ -0,0 +1,14 @@
+
+input("Please press any key to create an account")
+name = input("Enter your name: ")
+lastname = input("Enter your last name: ")
+year_of_birth = input("Enter your year of birth: ")
+status = input("Are you married or single? ")
+country = input("Where are you from? ")
+
+print("Your name: " + name)
+print("Your last name: " + lastname)
+print("Your year of birth: " + year_of_birth)
+print("Your status: " + status)
+print("You are from: " + country)
+print("Thank you for signing up!")
