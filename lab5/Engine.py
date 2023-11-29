2bcbf1679fd1fbbb421e1eac8d50672fe766c518
Madina <abdulgafarova.madina@mail.ru>
2023-11-29 22:42:34 +0600

    Added lab5 directory and 2 classes

---
+++ lab5/Engine.py
@@ -0,0 +1,4 @@
+class Engine:
+    def __init__(self, power, manufacturer):
+        self.power = power
+        self.manufacturer = manufacturer
---
+++ lab5/Person.py
@@ -0,0 +1,4 @@
+class Person:
+    def __init__(self, full_name, driving_experience):
+        self.full_name = full_name
+        self.drinig_experience = driving_experience
