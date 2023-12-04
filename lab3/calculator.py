1e8b8ebfadfbe6a52875cf401a9a59ad24da9912
Madina <m_abdulgafar@kbtu.kz>
2023-12-04 19:25:52 +0600

    Merged master into main

--- lab3/calculator.py
+++ lab3/calculator.py
@@ -3,11 +3,8 @@ def add(x, y):
 
 def subtract(x, y):
     return x - y
-
 def multiply(x, y):
     return x * y
-
-
 def divide(x, y):
     if y == 0:
         return "Ошибка: деление на ноль недопустимо"
